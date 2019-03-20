4f620bd7f28473c59ceba70235b019a3e4ed0de7
chenchen32 <48753878+chenchen32@users.noreply.github.com>
2019-03-20 22:40:29 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/music/precache-manifest.c624721c7f4f2b434c20ba36625d1304.js"
+  "/precache-manifest.c624721c7f4f2b434c20ba36625d1304.js"
 );
 
 workbox.clientsClaim();
