a7665db1a5681ec000b776de2d8eab5df308a07b
chenchen32 <48753878+chenchen32@users.noreply.github.com>
2019-03-20 22:33:19 +0800

    Update precache-manifest.c624721c7f4f2b434c20ba36625d1304.js
    修改路径

--- precache-manifest.c624721c7f4f2b434c20ba36625d1304.js
+++ precache-manifest.c624721c7f4f2b434c20ba36625d1304.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "5e39c52062f2e8c0e256",
-    "url": "/music/static/css/main.80c04495.chunk.css"
+    "url": "/static/css/main.80c04495.chunk.css"
   },
   {
     "revision": "5e39c52062f2e8c0e256",
-    "url": "/music/static/js/main.5e39c520.chunk.js"
+    "url": "/static/js/main.5e39c520.chunk.js"
   },
   {
     "revision": "d67700ee5d4cb03bd0de",
-    "url": "/music/static/js/1.d67700ee.chunk.js"
+    "url": "/static/js/1.d67700ee.chunk.js"
   },
   {
     "revision": "5604c427e1d539a6c107",
-    "url": "/music/static/js/runtime~main.5604c427.js"
+    "url": "/static/js/runtime~main.5604c427.js"
   },
   {
     "revision": "e4c91660ac34a950921100691d68f380",
-    "url": "/music/index.html"
+    "url": "/index.html"
   }
 ];
