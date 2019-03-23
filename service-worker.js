2df2ea154b304b70e3f9ee888b07b80e1fd3792a
chenchen32 <48753878+chenchen32@users.noreply.github.com>
2019-03-23 13:04:48 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/music/precache-manifest.ba30dfb55fc53d2c2b5fb2fa1c23b45f.js"
+  "/precache-manifest.ba30dfb55fc53d2c2b5fb2fa1c23b45f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/music/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
