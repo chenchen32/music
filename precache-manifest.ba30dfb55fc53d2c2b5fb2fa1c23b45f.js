6ab65acc3bdee15aa2420dff3fbdc6602ad36c87
chenchen32 <48753878+chenchen32@users.noreply.github.com>
2019-03-23 13:04:15 +0800

    Update precache-manifest.ba30dfb55fc53d2c2b5fb2fa1c23b45f.js

--- precache-manifest.ba30dfb55fc53d2c2b5fb2fa1c23b45f.js
+++ precache-manifest.ba30dfb55fc53d2c2b5fb2fa1c23b45f.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "d89ee803d92206175644",
-    "url": "/music/static/css/main.50df2e78.chunk.css"
+    "url": "/static/css/main.50df2e78.chunk.css"
   },
   {
     "revision": "d89ee803d92206175644",
-    "url": "/music/static/js/main.d89ee803.chunk.js"
+    "url": "/static/js/main.d89ee803.chunk.js"
   },
   {
     "revision": "d67700ee5d4cb03bd0de",
-    "url": "/music/static/js/1.d67700ee.chunk.js"
+    "url": "/static/js/1.d67700ee.chunk.js"
   },
   {
     "revision": "5604c427e1d539a6c107",
-    "url": "/music/static/js/runtime~main.5604c427.js"
+    "url": "/static/js/runtime~main.5604c427.js"
   },
   {
     "revision": "cdbaab647d54fd60aefce2a1acec0297",
-    "url": "/music/index.html"
+    "url": "/index.html"
   }
 ];
