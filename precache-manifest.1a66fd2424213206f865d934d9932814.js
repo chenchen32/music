6d9db3acf053eb5734eaf4f04008675588b45269
qianc <qianchengcn@foxmail.com>
2019-08-01 23:44:15 +0800

    Updates

---
+++ precache-manifest.1a66fd2424213206f865d934d9932814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2606e0807227bb5bf62a",
+    "url": "/music/static/css/main.1e51b215.chunk.css"
+  },
+  {
+    "revision": "2606e0807227bb5bf62a",
+    "url": "/music/static/js/main.2606e080.chunk.js"
+  },
+  {
+    "revision": "77f7a49382e7fc314648",
+    "url": "/music/static/js/1.77f7a493.chunk.js"
+  },
+  {
+    "revision": "5604c427e1d539a6c107",
+    "url": "/music/static/js/runtime~main.5604c427.js"
+  },
+  {
+    "revision": "eafacb98566c25172e0e67bf5c241fed",
+    "url": "/music/index.html"
+  }
+];
